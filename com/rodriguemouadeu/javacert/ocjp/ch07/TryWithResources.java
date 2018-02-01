ab0b2539bf202ca862534a63ef368fe8b5cfce8a
Rodrigue Mouadeu <rodrigue.mouadeu@gmail.com>
2018-02-01 12:15:34 -0500

    Add missing file

--- com/rodriguemouadeu/javacert/ocjp/ch07/TryWithResources.java
+++ com/rodriguemouadeu/javacert/ocjp/ch07/TryWithResources.java
@@ -1,28 +1,16 @@
 package com.rodriguemouadeu.javacert.ocjp.ch07;
 
 public class TryWithResources {
-	
+
 	public static void main(String[] args){
-	
+
 		try(One one = new One(); Two two = new Two()){
-			System.out.println("Try");			
+			System.out.println("Try");
 			throw new RuntimeException();
-		} catch(Exception e){ 
+		} catch(Exception e){
 			System.out.println("Catch");
 		} finally {
 			System.out.println("Finally");
 		}
-	}
-}
-
-class One implements AutoCloseable {
-	public void close(){
-		System.out.println("Close One");
-	}
-}
-
-class Two implements AutoCloseable {
-	public void close(){
-		System.out.println("Close Two");
 	}
 }
